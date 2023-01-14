6e2529f249c667029576dc53164753ff3f3b7000
mruqqie <mruqqie@gmail.com>
2023-01-14 12:02:12 -0800

    modified:   src/firebase.js
    	modified:   src/firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -19,11 +19,3 @@ export const storage = getStorage();
 export const db = getFirestore(app);
 
 
-// rules_version = '2';
-// service cloud.firestore {
-//   match /databases/{database}/documents {
-//     match /{document=**} {
-//       allow read, write: if false;
-//     }
-//   }
-// }
